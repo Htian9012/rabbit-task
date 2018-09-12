853377bce95556329bdd551babab5ebfd4bd34b7
641801732@qq.com <641801732@qq.com>
2018-09-12 21:09:42 +0800

    update

--- src/main/java/com/runner/TestRunner.java
+++ src/main/java/com/runner/TestRunner.java
@@ -2,6 +2,8 @@ package com.runner;
 
 import com.amq.base.TaskData;
 import com.amq.base.TaskRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 /**
  * runner类，执行mq队列分发的任务
@@ -9,9 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestRunner extends TaskRunner<String,String> {
 
+    private static final Logger log = LoggerFactory.getLogger(TestRunner.class);
+
     @Override
     public String runTask(TaskData<String, String> taskData) throws Exception {
-        System.out.println("模拟业务处理参数"+taskData.getTaskParam());
-        return "处理成功:返回结果";
+        log.info("模拟业务处理,参数:{} ",taskData.getTaskParam());
+        return "处理成功,返回结果";
     }
 }
